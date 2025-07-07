1987093cad73032a73879d8e3d950b8b7c301f9c
Catalyst-235 <81960015+Catalyst-235@users.noreply.github.com>
2025-07-07 13:27:36 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'barcode-generator-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
